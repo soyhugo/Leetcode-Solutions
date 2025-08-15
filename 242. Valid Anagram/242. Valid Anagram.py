98f197923219ba067e16be94139299cde11249b9
Hugo <hl865@drexel.edu>
2025-08-16 00:48:43 +0200

    .

--- 242. Valid Anagram/242. Valid Anagram.py
+++ 242. Valid Anagram/242. Valid Anagram.py
@@ -20,5 +20,29 @@ class Solution:
       if freq_map[char] != 0:
         return False
     return True
+  
+""""
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        freq_s = {}
+        freq_t = {}
+        for char in s:
+            if char in freq_s:
+                freq_s[char] += 1
+            else:
+                freq_s[char] = 1
+        for char in t:
+            if char in freq_t:
+                freq_t[char] += 1
+            else:
+                freq_t[char] = 1
 
+        if freq_s == freq_t:
+            return True
+        else:
+            return False
+
+"""
         
