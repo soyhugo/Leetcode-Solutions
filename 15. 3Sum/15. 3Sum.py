94a84f8d86b81ea34bb088653f76dfc2b0e9cb6d
Hugo <hl865@drexel.edu>
2025-11-20 11:03:34 -0500

    3sum again

--- 15. 3Sum/15. 3Sum.py
+++ 15. 3Sum/15. 3Sum.py
@@ -1,53 +1,36 @@
-#first solution:
-class Solution(object):
-    def threeSum(self, nums):
-        #sort list
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        #create sol list
         solution = []
-        #main loop
-        for i in range(0, len(nums) - 2):
-            fixed_number = nums[i]
-            left, right = i+1 , len(nums) - 1
-            while left < right:
-                if fixed_number + nums[left] + nums[right] == 0:
-                    if not [fixed_number, nums[left], nums[right]] in solution:
-                        solution.append([fixed_number, nums[left], nums[right]])
-                    right -= 1
-                    left += 1
-                elif fixed_number + nums[left] + nums[right] > 0:
-                    right -= 1
-                elif fixed_number + nums[left] + nums[right] < 0:
-                    left += 1
-        return solution
+        n = len(nums)
 
-        
-
-#Optimized solution:
-class Solution(object):
-    def threeSum(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-        res=[]
-        nums.sort()
-        for i ,a  in enumerate(nums):
-            if i>0  and a==nums[i-1]:
+        left = 0
+        while left < n - 2:
+            if left > 0 and nums[left] == nums[left - 1]:
+                left += 1
                 continue
-            l,r=i+1,len(nums)-1
-            while l<r:
-                total=a+nums[l]+nums[r]
-                if total>0:
-                    r-=1
-                elif total<0:
-                    l+=1
+
+            middle = left + 1
+            right = n - 1
+
+            while middle < right:
+                current = nums[left] + nums[middle] + nums[right]
+
+                if current == 0:
+                    solution.append([nums[left], nums[middle], nums[right]])
+                    middle += 1
+                    right -= 1
+
+                    # FIX: skip duplicates
+                    while middle < right and nums[middle] == nums[middle - 1]:
+                        middle += 1
+                    while middle < right and nums[right] == nums[right + 1]:
+                        right -= 1
+
+                elif current < 0:
+                    middle += 1
                 else:
-                    res.append([a,nums[l],nums[r]])
-                    l+=1
-                    r-=1
-                    while l<r and nums[l]==nums[l-1]:
-                        l+=1
-                    while l<r and nums[r]==nums[r+1]:
-                        r-=1
-        return res
+                    right -= 1
+
+            left += 1
+        return solution
